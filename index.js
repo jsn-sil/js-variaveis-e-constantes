e2e6e76f2c446475120ca2455aa293933d1fb469
jsn-sil <joelsonid@gmail.com>
2024-09-18 12:05:49 -0300

    js

---
+++ index.js
@@ -0,0 +1,88 @@
+let valor = 10; // number
+let nome_completo = 'Nayara' // string
+let nomeCompleto = 'Ceasar' // string
+let NomeCompleto = 'Daniel' // string
+let verdadeiro = true; // boolean
+let falso = false; // boolean
+
+document.writeln (valor)
+valor = 20;
+document.writeln(valor);
+valor = 30;
+document.writeln (valor);
+
+document.writeln(valor);
+document.writeln(nome_completo);
+document.writeln(NomeCompleto);
+document.writeln(NomeCompleto);
+document.writeln(verdadeiro);
+document.writeln(falso);
+
+const nomeDaTurma = 'FS18';
+document.write("<br>");
+document.write(nomeDaTurma);
+
+// Operadores de atribuição
+// =, +=, -= 
+let idade = 34;
+document.write("<br>");
+document.write(idade);
+idade += 1;
+document.write("<br>");
+document.write(idade);
+
+
+// operadores aritméticos
+// +, -, *, /, %
+document.write("<br>");
+document.write(2+1);
+document.write("<br>");
+document.write(4-1);
+document.write("<br>");
+document.write(2*7);
+document.write("<br>");
+document.write(7/9);
+document.write("<br>");
+document.write(2%7);
+
+// operadores de comparação
+// ==, !=, ===, !==, <, >, <=, >=,
+document.write("<br>");
+document.write(2==2);
+document.write("<br>");
+document.write(2!=2);
+document.write("<br>");
+document.write(2===2);
+document.write("<br>");
+document.write(2===2);
+document.write("<br>")
+document.write(2==='2');
+document.write("<br>");
+document.write(2!==2); //diferentes no valor e no tipo
+document.write("<br>")
+document.write(2!=='2'); //é diferente valor e o tipo 
+
+
+document.write("<br>");
+document.write(2>2);
+document.write("<br>");
+document.write(2<2);
+document.write("<br>");
+document.write(2<=2);
+document.write("<br>");
+document.write(2>=2);
+
+//operadores lógicos
+// !, !!, &&, ||
+document.write("<br>");
+document.write(!true);
+document.write("<br>");
+document.write(!!3);
+document.write("<br>");
+document.write(3==3 && 2==2);
+document.write("<br>");
+document.write(3==3 && 2==3);
+document.write("<br>");
+document.write(3==4 && 2==2);
+document.write("<br>");
+document.write(3!=3 && 2>=2);
---
+++ readme.md
@@ -0,0 +1,25 @@
+# javascript variáveis e constantes
+
+### variáveis
+
+- É um espaço na memoria do computador para armazenar uma informação/valor que pode ser alterado posteriomente
+
+#### padrões de nomeclatura
+
+- o nome não pode começar com numeros ou caractéres alfanuméricos
+
+- de preferência tudo minusculo, mas se for uma palavra composta utilize um dos padrões abaixo:
+
+- camelCase: começa com letra minuscula e coloca-se uma maiuscula  a cada nome, ex -> nomeComposto
+
+- snake_case: escreve tudo minusculo e adiciona um underline para separar as palavras, -> ex_composto
+
+- PascalCase: começa com letra maiuscula e a cada palavra adiciona mais uma letra maiuscula, ex -> NomeComposto
+
+### Constantes
+
+- é um espaço da memoria que armazena um valor, mas não pode ser alterado!
+
+### Tipos de dados
+
+- variáveis e constantes possuem tipos, e os principais são: string, nuber e boolean
